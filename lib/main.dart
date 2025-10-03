745712ce2d0122c8ecbebc33fffc8121cd070885
KeithEmpasis21 <wempasis_230000000674@uic.edu.ph>
2025-10-03 23:39:08 +0800

    applied firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'onboarding.dart';
 
+
 void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(const ByteBackApp());
 }
 
