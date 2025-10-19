514f115501037f732369724cf5d1a35e037b95c6
KeithEmpasis21 <wempasis_230000000674@uic.edu.ph>
2025-10-20 03:24:01 +0800

    fixed firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'onboarding.dart';
-
+import 'firebase_options.dart';
 
 void main() async {
 
